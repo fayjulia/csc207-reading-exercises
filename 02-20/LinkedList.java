e1563f0934ee939854ed6d6baa590597f3e1673d
Julia Fay <juliafay@Julias-MacBook-Air-2.local>
2017-02-19 21:41:36 -0600

    added some functionality

--- 02-20/LinkedList.java
+++ 02-20/LinkedList.java
@@ -1,7 +1,24 @@
+import Node;
+
 public class LinkedList {
 
     public LinkedList (Node first) {
 	this.first = first;
+    }
+
+    public void addFirst (LinkedList list, Node first) {
+	Node n = new Node(first, list.first);
+	list.first = n;
+    }
+
+    public int size (LinkedList list) {
+	int sz = 0;
+	Node cur = list.first;
+	while (cur != NULL) {
+	    sz++;
+	    cur = cur.next;
+	}
+	return sz;
     }
 
 }
